5dff0b8db70d995a6e3cce568872e682d043aece
Angela <angelikatyburska@gmail.com>
2020-02-29 18:40:06 +0100

    Added displaying of selected syllabary

--- src/Components/SingleSign.js
+++ src/Components/SingleSign.js
@@ -2,12 +2,22 @@ import React, { Component } from "react";
 
 class SingleSign extends Component {
   render() {
-    return (
-      <div className="sign-box">
-        <p className="sign-box__character"></p>
-        <span className="sign-box__meaning"></span>
-      </div>
-    );
+    const { kanaTable, syllabary } = this.props;
+    if (syllabary === "hiragana") {
+      return (
+        <div className="sign-box">
+          <p className="sign-box__character">{kanaTable.hiragana}</p>
+          <span className="sign-box__meaning">{kanaTable.meaning}</span>
+        </div>
+      );
+    } else {
+      return (
+        <div className="sign-box">
+          <p className="sign-box__character">{kanaTable.katakana}</p>
+          <span className="sign-box__meaning">{kanaTable.meaning}</span>
+        </div>
+      );
+    }
   }
 }
 
