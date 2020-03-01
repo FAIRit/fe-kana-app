f6684459392e555d6197e473175a558f806b74f0
Angela <angelikatyburska@gmail.com>
2020-03-01 14:29:40 +0100

    Modified CheatScheet component and SingleSign component's props

--- src/Components/SingleSign.js
+++ src/Components/SingleSign.js
@@ -2,19 +2,19 @@ import React, { Component } from "react";
 
 class SingleSign extends Component {
   render() {
-    const { kanaTable, syllabary } = this.props;
+    const { kanaTable, syllabary, kanaMeaning } = this.props;
     if (syllabary === "hiragana") {
       return (
         <div className="sign-box">
-          <p className="sign-box__character">{kanaTable.hiragana}</p>
-          <span className="sign-box__meaning">{kanaTable.meaning}</span>
+          <p className="sign-box__character">{kanaTable}</p>
+          <span className="sign-box__meaning">{kanaMeaning}</span>
         </div>
       );
     } else {
       return (
         <div className="sign-box">
-          <p className="sign-box__character">{kanaTable.katakana}</p>
-          <span className="sign-box__meaning">{kanaTable.meaning}</span>
+          <p className="sign-box__character">{kanaTable}</p>
+          <span className="sign-box__meaning">{kanaMeaning}</span>
         </div>
       );
     }
