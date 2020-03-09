e3d81f60fc29c18d5cb5f2238e86cc358362cf69
Angela <angelikatyburska@gmail.com>
2020-03-09 09:53:51 +0100

    Installed Material UI, added reset to App component

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import "./scss/main.scss";
+// import "./scss/main.scss";
 
 ReactDOM.render(
   <Router>
