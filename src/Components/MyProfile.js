c6da5a5d698889982a91a663d8b98aef35327d2c
Angela <angelikatyburska@gmail.com>
2020-03-26 16:08:27 +0100

    Cleanup

--- src/Components/MyProfile.js
+++ src/Components/MyProfile.js
@@ -1,14 +1,11 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
-import { Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 import Grid from "@material-ui/core/Grid";
 import { styled } from "@material-ui/core/styles";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import Input from "@material-ui/core/Input";
-import { Link } from "react-router-dom";
-import { keepDataInLocalStorage } from "../Redux/actions/auth";
-import { storage } from "../Firebase/firebase";
 
 import firebase from "firebase";
 
@@ -29,6 +26,8 @@ const getExtension = name => {
   const lastDot = name.lastIndexOf(".");
   return name.substring(lastDot + 1);
 };
+
+const noop = () => {};
 
 class MyProfile extends Component {
   state = {
@@ -51,7 +50,7 @@ class MyProfile extends Component {
       image
     });
 
-    const storageRef = storage.ref();
+    const storageRef = firebase.storage().ref();
     const imageRef = storageRef.child(`avatars/${uid}.${ext}`);
 
     const uploadTask = imageRef.put(image);
@@ -72,15 +71,15 @@ class MyProfile extends Component {
     });
   };
 
-  unsubscribeFromAuth = null;
-  unsubscribeFromUserProfile = null;
+  unsubscribeFromAuth = noop;
+  unsubscribeFromUserProfile = noop;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = firebase.auth().onAuthStateChanged(user => {
+    const handleAuthChange = user => {
       console.log(user);
       if (user) {
         const { uid, email } = user;
-        console.log(uid, email)
+        console.log(uid, email);
 
         this.unsubscribeFromUserProfile = firebase
           .database()
@@ -106,71 +105,52 @@ class MyProfile extends Component {
           user: null
         });
       }
-    });
+    };
+
+    this.unsubscribeFromAuth = firebase
+      .auth()
+      .onAuthStateChanged(handleAuthChange);
   }
 
   componentWillUnmount() {
-    if (this.unsubscribeFromAuth) {
-      this.unsubscribeFromAuth();
-    }
+    this.unsubscribeFromAuth();
   }
 
   render() {
-    const { isAuthenticated } = this.props;
     const { user } = this.state;
-    if (!isAuthenticated) {
-      return <Redirect to="/" />;
-    } else {
-      return (
-        <OuterGrid
-          container
-          direction="column"
-          justify="center"
-          alignItems="stretch"
-        >
-          <section className="my-profile">
-            <main className="my-profile__container">
-              <div className="my-profile__avatar-box">
-                <Avatar src={user?.avatarUrl} />
-                <StyledInput
-                  type="file"
-                  accept="image/*"
-                  id="upload-image"
-                  multiple={false}
-                  onChange={this.handleSelectImage}
-                />
-                <label htmlFor="upload-image">
-                  <Button variant="contained" component="span">
-                    Zmień avatar
-                  </Button>
-                </label>
-              </div>
-              <Button variant="contained" component={Link} to="/home">
-                Powrót
-              </Button>
-            </main>
-          </section>
-        </OuterGrid>
-      );
-    }
+
+    return (
+      <OuterGrid
+        container
+        direction="column"
+        justify="center"
+        alignItems="stretch"
+      >
+        <section className="my-profile">
+          <main className="my-profile__container">
+            <div className="my-profile__avatar-box">
+              <Avatar src={user?.avatarUrl} />
+              <StyledInput
+                type="file"
+                accept="image/*"
+                id="upload-image"
+                multiple={false}
+                onChange={this.handleSelectImage}
+              />
+              <label htmlFor="upload-image">
+                <Button variant="contained" component="span">
+                  Zmień avatar
+                </Button>
+              </label>
+            </div>
+            <Button variant="contained" component={Link} to="/home">
+              Powrót
+            </Button>
+          </main>
+        </section>
+      </OuterGrid>
+    );
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    isAuthenticated: state.auth.isAuthenticated,
-    imageUrl: state.auth.imageUrl,
-    imageName: state.auth.imageName,
-    user: state.auth.user
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    uploadImageUrl: (imageUrl, imageName) => {
-      dispatch(keepDataInLocalStorage(imageUrl, imageName));
-    }
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(MyProfile);
+export default MyProfile
